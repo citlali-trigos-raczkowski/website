bc80894270d158ad7aea2a516268a01b757ddbfc
Citlali Trigos <cbtrigos@mit.edu>
2020-08-20 22:43:44 -0700

    Updates

---
+++ precache-manifest.4d9645d8596528a826f9ca4f964714c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc01106be8f2298891d907f6d4a2c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0faf3722a2eae028552a",
+    "url": "/static/css/2.f3cadcfb.chunk.css"
+  },
+  {
+    "revision": "c8a37d0a34dcb4f71ec2",
+    "url": "/static/css/main.949b1a65.chunk.css"
+  },
+  {
+    "revision": "0faf3722a2eae028552a",
+    "url": "/static/js/2.c41f9d3d.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/static/js/2.c41f9d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a37d0a34dcb4f71ec2",
+    "url": "/static/js/main.22bafadc.chunk.js"
+  },
+  {
+    "revision": "f1bbdf686f341db31e24",
+    "url": "/static/js/runtime-main.9c148f23.js"
+  },
+  {
+    "revision": "6e53d3426738a03adccbe10c8ffe9005",
+    "url": "/static/media/resume.6e53d342.pdf"
+  }
+]);
