5c4f2c34b49e50914df378cb606410af6537d2c5
Citlali Trigos <cbtrigos@mit.edu>
2020-08-20 22:47:14 -0700

    Updates

---
+++ precache-manifest.404d8829aea480c0383ecaae3e4a91db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a66d9de79bca745efe2c4fa9c378560",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a7cbdabe148ecd6c25bc",
+    "url": "/home/static/css/2.f3cadcfb.chunk.css"
+  },
+  {
+    "revision": "2c85aafe11e2cd02368b",
+    "url": "/home/static/css/main.949b1a65.chunk.css"
+  },
+  {
+    "revision": "a7cbdabe148ecd6c25bc",
+    "url": "/home/static/js/2.00b9c848.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/home/static/js/2.00b9c848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c85aafe11e2cd02368b",
+    "url": "/home/static/js/main.c38e28bc.chunk.js"
+  },
+  {
+    "revision": "c3d0b23eba7d10bf3664",
+    "url": "/home/static/js/runtime-main.bc3c8a27.js"
+  },
+  {
+    "revision": "6e53d3426738a03adccbe10c8ffe9005",
+    "url": "/home/static/media/resume.6e53d342.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25026f87c418191dcc3a45ed0b0521a1.js"
+  "/home/precache-manifest.404d8829aea480c0383ecaae3e4a91db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
